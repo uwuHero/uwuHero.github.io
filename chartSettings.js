35c04288579bc86e9bb5bc74236a9f9575b753f5
Edward Haas <efhiii2000@gmail.com>
2021-06-13 11:02:57 -0700

    names

--- chartSettings.js
+++ chartSettings.js
@@ -25,10 +25,13 @@ function drawChartSettings(x, y, w, h) {
 
     let mins = 0;
     for(let i = 0; i < cSong.tracks.length; i++) {
-      if(cSong.tracks[i].instrument.percussion){mins++;continue;}
+      if(cSong.tracks[i].instrument.percussion) {
+        mins++;
+        continue;
+      }
       button(
         (x + w * 0.1) >> 0,
-        (y + w * 0.1 * ((i-mins) + 1) + instrumentScroll * w) >> 0,
+        (y + w * 0.1 * ((i - mins) + 1) + instrumentScroll * w) >> 0,
         w * 0.5,
         w * 0.08,
         a => {
@@ -37,20 +40,13 @@ function drawChartSettings(x, y, w, h) {
           startSong();
           sb = 4;
         }, paper, paper);
-        /*
-      ctx.fillStyle = '#fffa';
-      ctx.fillRect(
-        (x + w * 0.1) >> 0,
-        (y + w * 0.1 * (i + 1) + instrumentScroll * w) >> 0,
-        w * 0.5,
-        w * 0.08);
-        */
+
       ctx.fillStyle = '#000';
       let txt = (i > 0 && cSong.tracks[i - 1].instrument.name == cSong.tracks[i].instrument.name ? (cSong.tracks[i].notes[0].midi < 60 ? 'bass ' : '') : '') + (cSong.tracks.length > 6 ? cSong.tracks[i].instrument.name : cSong.tracks[i].instrument.family);
       ctx.font = `${(txt.length>25?txt.length>33?w*0.02:w*0.03:w*0.04)>>0}px sans-serif`;
-      ctx.fillText(' '+txt,
+      ctx.fillText(' ' + txt,
         (x + w * 0.11) >> 0,
-        (y + w * 0.1 * ((i-mins) + (txt.length > 25 ? txt.length > 33 ? 1.5 : 1.55 : 1.6)) + instrumentScroll * w) >> 0);
+        (y + w * 0.1 * ((i - mins) + (txt.length > 25 ? txt.length > 33 ? 1.5 : 1.55 : 1.6)) + instrumentScroll * w) >> 0);
     }
   } else {
     ctx.drawImage(background, x, y, w, h);
--- settings.js
+++ settings.js
@@ -21,6 +21,84 @@ let keyBindings = {
 
 let binding = -1;
 
+var keyCodes={
+  3:'break',
+  8:'back',
+  9:'tab',
+  12:'clear',
+  13:'enter',
+  16:'shift',
+  17:'crtl',
+  18:'alt',
+  19:'pause',
+  20:'caps',
+  21:'hangul',
+  25:'hanja',
+  28:'cnvrsn',
+  29:'n-cnvrsn',
+  32:'space',
+  33:'pg up',
+  34:'pg dn',
+  35:'end',
+  36:'home',
+  37:'left',
+  38:'up',
+  39:'right',
+  40:'down',
+  41:'slct',
+  42:'prnt',
+  43:'exe',
+  44:'prt scn',
+  45:'ins',
+  46:'del',
+  47:'help',
+  91:'lwin',
+  92:'rwin',
+  93:'rcmd',
+  95:'sleep',
+  96:'np 0',
+  97:'np 1',
+  98:'np 2',
+  99:'np 3',
+  100:'np 4',
+  101:'np 5',
+  102:'np 6',
+  103:'np 7',
+  104:'np 8',
+  105:'np 9',
+  160:'^',
+  161:'!',
+  162:';',
+  163:'#',
+  164:'$',
+  165:'u',
+  172:'home',
+  193:'?',
+  223:'`',
+
+  188:',',
+  186:';',
+  187:'=',
+  189:'-',
+  190:'.',
+  191:'/',
+  192:'`',
+  194:'.',
+  219:'[',
+  220:'\\',
+  221:']'
+};
+
+function keyName(code){
+  if(code>47 && code<91){
+    return String.fromCharCode(code);
+  }
+  if(keyCodes.hasOwnProperty(code)){
+    return keyCodes[code];
+  }
+  return code;
+}
+
 function drawSettings(x, y, w, h) {
   ctx.drawImage(background, x, y, w, h);
 
@@ -28,26 +106,44 @@ function drawSettings(x, y, w, h) {
   ctx.textAlign = 'center center';
   ctx.font = `${(w*0.04)>>0}px sans-serif`;
 
-  ctx.fillText('Lanes', x + w * 0.05, y + h * 0.38);
-  ctx.fillText(frets, x + w * 0.25, y + h * 0.38);
+  ctx.fillText('Keys', x + w * 0.45, y + h * 0.78);
 
-
-  ctx.fillText('Keys', x + w * 0.45, y + h * 0.48);
+  ctx.fillText('Lanes', x + w * 0.05, y + h * 0.23);
+  ctx.fillText(frets, x + w * 0.45, y + h * 0.23);
 
   button(
-    x + w * 0.18,
-    y + h * 0.3 + w * 0.01,
+    x + w * 0.38,
+    y + h * 0.15 + w * 0.01,
     w * 0.04, w * 0.04,
     a => {
       frets = Math.max(2, frets - 1)
     }, minus, minus);
 
   button(
-    x + w * 0.3,
-    y + h * 0.3 + w * 0.01,
+    x + w * 0.5,
+    y + h * 0.15 + w * 0.01,
     w * 0.04, w * 0.04,
     a => {
       frets = Math.min(fretColors.length, frets + 1)
+    }, plus, plus);
+
+  ctx.fillText('Max Chord Size', x + w * 0.05, y + h * 0.33);
+  ctx.fillText(maxNotes, x + w * 0.45, y + h * 0.33);
+
+  button(
+    x + w * 0.38,
+    y + h * 0.25 + w * 0.01,
+    w * 0.04, w * 0.04,
+    a => {
+      maxNotes = Math.max(1, maxNotes - 1)
+    }, minus, minus);
+
+  button(
+    x + w * 0.5,
+    y + h * 0.25 + w * 0.01,
+    w * 0.04, w * 0.04,
+    a => {
+      maxNotes = Math.min(frets, maxNotes + 1)
     }, plus, plus);
 
 
@@ -60,16 +156,22 @@ function drawSettings(x, y, w, h) {
   for(let i = 0; i < frets; i++) {
     button(
       x + w * 0.05 + w * 0.98 * i / frets - w * 0.04,
-      y + h * 0.5 + w * 0.01,
+      y + h * 0.8 + w * 0.01,
       w * 0.08, w * 0.08,
       a => {
         binding = i;
         keyBindings.notes[i] = -1;
       }, minus, minus);
 
-    ctx.fillText(keyBindings.notes[i],
-      x + w * 0.05 + w * 0.98 * i / frets - h * 0.05,
-      y + h * 0.75);
+    ctx.fillStyle = fretColors[i];
+    ctx.fillRect(
+      x + w * 0.05 + w * 0.98 * i / frets - w * 0.04,
+      y + h * 0.8 + w * 0.01,
+      w * 0.08, w * 0.04);
+
+    ctx.fillText(keyName(keyBindings.notes[i]),
+      x + w * 0.05 + w * 0.98 * i / frets - h * 0.04,
+      y + h * 0.95);
   }
 }
 
