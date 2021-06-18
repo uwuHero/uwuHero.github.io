aa08e877207b90757359a6617cb569d3b37c5e1b
Edward Haas <efhiii2000@gmail.com>
2021-06-18 12:30:13 -0700

    fullscreen

--- assetsList.js
+++ assetsList.js
@@ -4,6 +4,17 @@ background.src = 'assets/background.png';
 const overlay = new Image();
 overlay.src = 'assets/overlay.png';
 
+const fullscreen = new Image();
+fullscreen.src = 'assets/fullscreen.png';
+const fullscreenb = new Image();
+fullscreenb.src = 'assets/fullscreenb.png';
+
+const exitFullscreen = new Image();
+exitFullscreen.src = 'assets/exitFullscreen.png';
+const exitFullscreenb = new Image();
+exitFullscreenb.src = 'assets/exitFullscreenb.png';
+
+
 
 const uwu = new Image();
 uwu.src = 'assets/uwu.png';
--- menu.js
+++ menu.js
@@ -2,6 +2,16 @@ function drawMenu(x, y, w, h) {
   ctx.drawImage(background, x, y, w, h);
   button(x + w * 0.05, y + h * 0.3, w * 0.3, h * 0.15, a => sb = 2, playImg, playImgb)
   button(x + w * 0.05, y + h * 0.5, w * 0.3, h * 0.15, a => sb = 1, settingsImg, settingsImgb)
+
+  if(window.innerHeight == screen.height) {
+    button(x + 0.89 * w, y + h - 0.11 * w, 0.1 * w, 0.1 * w, () => {
+      document.exitFullscreen()
+    }, exitFullscreen, exitFullscreenb);
+  } else {
+    button(x + 0.89 * w, y + h - 0.11 * w, 0.1 * w, 0.1 * w, () => {
+      document.body.requestFullscreen()
+    }, fullscreen, fullscreenb);
+  }
 }
 
 function s0() {
